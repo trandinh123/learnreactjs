94df6182d70333c112371669cfe9d509feb80126
trandinh123 <dha8677@gmail.com>
2022-07-06 17:04:17 +0700

    commit

---
+++ src/components/TodoForm/TodoForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./TodoForm.css";
+export default function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
